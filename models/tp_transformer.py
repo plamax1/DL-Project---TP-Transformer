c8ba764d5b35aa2e0615662470c1ec911cf87499
plamax1 <pietro.lamanna98@gmail.com>
2023-03-06 12:04:05 +0100

    created tp_transformer model file

---
+++ models/tp_transformer.py
@@ -0,0 +1,22 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class PositionsEncoding(nn.Module):
+    def __init__(self, vocab_dim, embedding_dim, dropout, lenght):
+        super(PositionsEncoding,self).__init__
+        self.dropout = nn.Dropout(dropout)
+        self.lenght=lenght
+        self.embedding_dim = embedding_dim
+
+        #token encoding
+        self.embedding = nn.Embedding(vocab_dim, embedding_dim )
+        self.scaling = torch.sqrt(torch.FloatTensor([embedding_dim]))
+
+        #sinusoidal encoding
+        pos_enc = torch.zeros(lenght, embedding_dim)
+        # create a o matriz on lenght rows, one for each word, 
+        #and for each word create embedding_dim cols, for the encoding of each word
+        
