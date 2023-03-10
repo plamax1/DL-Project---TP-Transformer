2797e77063d43a0dc7ba99a43ab87585f464490b
plamax1 <pietro.lamanna98@gmail.com>
2023-03-10 17:52:52 +0100

    dataloader to fix meaning

--- test_dataset_loading.py
+++ test_dataset_loading.py
@@ -155,3 +155,162 @@ print('index to string: ',voc.itos)
 print('string to index:',voc.stoi)
 
 print('numericalize -> cat and a dog: ', voc.numericalize('cat and a dog'))
+
+
+class Train_Dataset(Dataset):
+    '''
+    Initiating Variables
+    df: the training dataframe
+    source_column : the name of source text column in the dataframe
+    target_columns : the name of target text column in the dataframe
+    transform : If we want to add any augmentation
+    freq_threshold : the minimum times a word must occur in corpus to be treated in vocab
+    source_vocab_max_size : max source vocab size
+    target_vocab_max_size : max target vocab size
+    '''
+    
+    def __init__(self, df, source_column, target_column, transform=None, freq_threshold = 0,
+                source_vocab_max_size = 10000, target_vocab_max_size = 10000):
+    
+        self.df = df
+        self.transform = transform
+        
+        #get source and target texts
+        self.source_texts = self.df[source_column]
+        self.target_texts = self.df[target_column]
+        
+        
+        ##VOCAB class has been created above
+        #Initialize source vocab object and build vocabulary
+        self.source_vocab = Vocabulary(freq_threshold, source_vocab_max_size)
+        self.source_vocab.build_vocabulary(self.source_texts.tolist())
+        #Initialize target vocab object and build vocabulary
+        self.target_vocab = Vocabulary(freq_threshold, target_vocab_max_size)
+        self.target_vocab.build_vocabulary(self.target_texts.tolist())
+        
+    def __len__(self):
+        return len(self.df)
+    
+    '''
+    __getitem__ runs on 1 example at a time. Here, we get an example at index and return its numericalize source and
+    target values using the vocabulary objects we created in __init__
+    '''
+    def __getitem__(self, index):
+        source_text = self.source_texts[index]
+        target_text = self.target_texts[index]
+        
+        if self.transform is not None:
+            source_text = self.transform(source_text)
+            
+        #numericalize texts ['<SOS>','cat', 'in', 'a', 'bag','<EOS>'] -> [1,12,2,9,24,2]
+        numerialized_source = [self.source_vocab.stoi["<SOS>"]]
+        numerialized_source += self.source_vocab.numericalize(source_text)
+        numerialized_source.append(self.source_vocab.stoi["<EOS>"])
+    
+        numerialized_target = [self.target_vocab.stoi["<SOS>"]]
+        numerialized_target += self.target_vocab.numericalize(target_text)
+        numerialized_target.append(self.target_vocab.stoi["<EOS>"])
+        
+        #convert the list to tensor and return
+        return torch.tensor(numerialized_source), torch.tensor(numerialized_target) 
+#######################################################
+#               Define Dataset Class
+#######################################################
+
+class Validation_Dataset:
+    def __init__(self, train_dataset, df, source_column, target_column, transform = None):
+        self.df = df
+        self.transform = transform
+        
+        #train dataset will be used as lookup for vocab
+        self.train_dataset = train_dataset
+        
+        #get source and target texts
+        self.source_texts = self.df[source_column]
+        self.target_texts = self.df[target_column]
+    
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self,index):
+        source_text = self.source_texts[index]
+        #print(source_text)
+        target_text = self.target_texts[index]
+        #print(target_text)
+        if self.transform is not None:
+            source_text = self.transform(source_text)
+            
+        #numericalize texts ['<SOS>','cat', 'in', 'a', 'bag','<EOS>'] -> [1,12,2,9,24,2]
+        numerialized_source = [self.train_dataset.source_vocab.stoi["<SOS>"]]
+        numerialized_source += self.train_dataset.source_vocab.numericalize(source_text)
+        numerialized_source.append(self.train_dataset.source_vocab.stoi["<EOS>"])
+    
+        numerialized_target = [self.train_dataset.target_vocab.stoi["<SOS>"]]
+        numerialized_target += self.train_dataset.target_vocab.numericalize(target_text)
+        numerialized_target.append(self.train_dataset.target_vocab.stoi["<EOS>"])
+        #print(numerialized_source)
+        return torch.tensor(numerialized_source), torch.tensor(numerialized_target) 
+#######################################################
+#               Collate fn
+#######################################################
+
+'''
+class to add padding to the batches
+collat_fn in dataloader is used for post processing on a single batch. Like __getitem__ in dataset class
+is used on single example
+'''
+
+class MyCollate:
+    def __init__(self, pad_idx):
+        self.pad_idx = pad_idx
+        
+    
+    #__call__: a default method
+    ##   First the obj is created using MyCollate(pad_idx) in data loader
+    ##   Then if obj(batch) is called -> __call__ runs by default
+    def __call__(self, batch):
+        #get all source indexed sentences of the batch
+        source = [item[0] for item in batch] 
+        #pad them using pad_sequence method from pytorch. 
+        source = pad_sequence(source, batch_first=False, padding_value = self.pad_idx) 
+        
+        #get all target indexed sentences of the batch
+        target = [item[1] for item in batch] 
+        #pad them using pad_sequence method from pytorch. 
+        target = pad_sequence(target, batch_first=False, padding_value = self.pad_idx)
+        return source, target
+
+#######################################################
+#            Define Dataloader Functions
+#######################################################
+
+# If we run a next(iter(data_loader)) we get an output of batch_size * (num_workers+1)
+def get_train_loader(dataset, batch_size, num_workers=0, shuffle=True, pin_memory=True): #increase num_workers according to CPU
+    #get pad_idx for collate fn
+    pad_idx = dataset.source_vocab.stoi['<PAD>']
+    #define loader
+    loader = DataLoader(dataset, batch_size = batch_size, num_workers = num_workers,
+                        shuffle=shuffle,
+                       pin_memory=pin_memory, collate_fn = MyCollate(pad_idx=pad_idx)) #MyCollate class runs __call__ method by default
+    return loader
+
+def get_valid_loader(dataset, train_dataset, batch_size, num_workers=0, shuffle=True, pin_memory=True):
+    pad_idx = train_dataset.source_vocab.stoi['<PAD>']
+    loader = DataLoader(dataset, batch_size = batch_size, num_workers = num_workers,
+                        shuffle=shuffle,
+                       pin_memory=pin_memory, collate_fn = MyCollate(pad_idx=pad_idx))
+    return loader
+
+
+train_dataset = Train_Dataset(data, 'Question', 'Answer')
+print(train.loc[1])
+train_dataset[1]
+
+train_loader = get_train_loader(train_dataset, 32)
+source = next(iter(train_loader))[0]
+target = next(iter(train_loader))[1]
+
+print('source: \n', source)
+
+print('source shape: ',source.shape)
+print('target shape: ', target.shape)
