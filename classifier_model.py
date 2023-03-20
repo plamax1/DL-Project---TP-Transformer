de875cbe5d34f8d83d57b7267893671dc1473eb5
plamax1 <pietro.lamanna98@gmail.com>
2023-03-20 18:11:04 +0100

    completed classifier model

--- classifier_model.py
+++ classifier_model.py
@@ -1,1 +1,27 @@
 print('Classifier')
+
+import torch
+import torch.nn as nn
+
+class Multiclass(nn.Module,  ):
+    def __init__(self, max_input_len, max_output_len, vocab_size):
+        super().__init__()
+        self.input = nn.Linear(max_input_len, 500)
+        self.act1 = nn.ReLU()
+        self.act2= nn.ReLU()
+        self.hidden = nn.Linear(500, 2000)
+        self.output = nn.Linear(2000, vocab_size*max_output_len)
+        self.sotfmax = nn.Softmax()
+        self.vocab_size = vocab_size
+        
+    def forward(self, x):
+        h1 = self.act1(self.input(x))
+        h2 = self.act2(self.input(h1))
+        out= h2.split(self.vocab_size)
+        result = []
+        for i in out:
+            result.append(torch.argmax(self.sotfmax(i)))
+            
+        return h2
+    
+
